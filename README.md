6bd32e9f26674a03817ad67286025b53c3596ebb
keoughcatelin <136517116+keoughcatelin@users.noreply.github.com>
2023-09-05 12:38:28 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# tech-blog
+# Tech-Blog
+
+## Description
+This web application is able to capture blogs such as comments, and posts, this web app uses handle bars, js, and css, as well as DB, this app was created for a way for users to sign in, and have a space where they can blog there tech journey or any types of tech info or tips,
+
+
+
+## Usage 
+Clone the code to your local system. add a .env file. npm i in the terminal, run mySQL then run node server and you will be able to view the application 
+
+## Credits 
+
+## license 
+MIT License
+
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ routes/api/product-routes.js
@@ -0,0 +1,113 @@
+const router = require("express").Router();
+const { Product, Category, Tag, ProductTag } = require("../../models");
+
+// The `/api/products` endpoint
+
+// get all products
+router.get("/", async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "no location found" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500), json(err);
+  }
+  // find all products
+  // be sure to include its associated Category and Tag data
+});
+
+// get one product
+router.get("/:id", async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag, through: ProductTag }],
+    });
+    if (!productData) {
+      res.status(404).json({ message: "no location found" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500), json(err);
+  } // find a single product by its `id`
+  // be sure to include its associated Category and Tag data
+});
+
+// create new product
+router.post("/", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+    if (req.body.tagIds.length) {
+      const productTagIdArr = req.body.tagIds.map((tag_id) => {
+        return {
+          product_id: product.id,
+          tag_id,
+        };
+      });
+      const productTagIds = await ProductTag.bulkCreate(productTagIdArr);
+      return res.status(200).json(productTagIds);
+    }
+    // if no product tags, just respond
+    res.status(200).json(product);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+// update product
+router.put("/:id", (req, res) => {
+  // update product data
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      if (req.body.tagIds && req.body.tagIds.length) {
+        ProductTag.findAll({
+          where: { product_id: req.params.id },
+        }).then((productTags) => {
+          // create filtered list of new tag_ids
+          const productTagIds = productTags.map(({ tag_id }) => tag_id);
+          const newProductTags = req.body.tagIds
+            .filter((tag_id) => !productTagIds.includes(tag_id))
+            .map((tag_id) => {
+              return {
+                product_id: req.params.id,
+                tag_id,
+              };
+            });
+
+          // figure out which ones to remove
+          const productTagsToRemove = productTags
+            .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+            .map(({ id }) => id);
+          // run both actions
+          return Promise.all([
+            ProductTag.destroy({ where: { id: productTagsToRemove } }),
+            ProductTag.bulkCreate(newProductTags),
+          ]);
+        });
+      }
+
+      return res.json(product);
+    })
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  // delete one product by its `id` value
+});
+
+module.exports = router;
